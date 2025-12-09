c0ed1b8f3be0769f4f333da712e7d5d708949fb8
RW Kopcke <rwkopckel@yahoo.com>
2025-12-09 18:12:46 -0500

    add immutable dicts to entry.py

--- src/sp500_earn_price_pkg/entry.py
+++ src/sp500_earn_price_pkg/entry.py
@@ -1,7 +1,8 @@
 import sys
+from types import MappingProxyType
 
-# import principal scripts
 from sp500_earn_price_pkg.principal_scripts import (
+        display_other_sp_indexes,
         update_data, 
         display_data,
         display_ind_data
@@ -10,15 +11,24 @@ from sp500_earn_price_pkg.principal_scripts import (
 
 def main():
     '''
-        Calls the three main scripts that produce this
+        Calls the main scripts that produce this
         project's data and displays
     '''
+    
+    def not_a_valid_key():
+        print(f'\n{action} is not a valid key')
 
-    action_dict = {
+    action_dict = MappingProxyType({
         "0": 'Update data from recent S&P and FRED workbooks',
         "1": 'Generate Displays for the S&P500 Index',
-        "2": 'Generate Displays for the S&P500 Industries'
-    }
+        "2": 'Generate Displays for the S&P500 Industries',
+        "3": 'Generate Displays for the 4 S&P Indexes'})
+    
+    function_dict = MappingProxyType({
+        "0": update_data.update,
+        "1": display_data.display,
+        "2": display_ind_data.display_ind,
+        "3": display_other_sp_indexes.display_spdexes})
 
     # request actions from user (cli)
     while True:
@@ -28,22 +38,13 @@ def main():
             print(f'{key}: {action_dict[key]}')
 
         action = input(
-            '\nEnter the key shown above for the intended action: ')
+            '\nEnter the key for the intended action: ')
         
-        match action:
-            case "0":
-                update_data.update()
-            case "1":
-                display_data.display()
-            case "2":
-                display_ind_data.display_ind()
-            case _:
-                print(f'{action} is not a valid key')
+        function_dict.get(action, not_a_valid_key)()
                 
         choice = input(
             '\nTo take another action, type T; otherwise, type F: ')
         
-        # quit if choice does not conform
         if choice not in ['T', 't', 'True', 'Y', 'y', 'yes']:
             print(f'{choice} End process.')
             sys.exit()
---
+++ src/sp500_earn_price_pkg/principal_scripts/display_other_sp_indexes.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+def display_spdexes():
+    print('code to come')
+    pass
+
